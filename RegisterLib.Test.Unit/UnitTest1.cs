ce6a85d9b7b15ffd72145be2c7b849a0cb33127c
Frank Bodholdt Jakobsen <au237297@uni.au.dk>
2020-08-03 15:53:18 +0200

    Add 2 tests for multiple add

--- RegisterLib.Test.Unit/UnitTest1.cs
+++ RegisterLib.Test.Unit/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace RegisterLib.Test.Unit
         }
 
         [Test]
-        public void Test1()
+        public void Add_NegativeItem_Throws()
         {
             Assert.That(
                 () => uut_.AddItem(-1),
@@ -34,5 +34,21 @@ namespace RegisterLib.Test.Unit
             uut_.AddItem(5.5);
             Assert.That(uut_.Total, Is.EqualTo(5.5));
         }
+
+        public void Add_TwoItems_CountIsCorrect()
+        {
+            uut_.AddItem(1.2);
+            uut_.AddItem(2.6);
+            Assert.That(uut_.NItems, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Add_TwoItems_TotalIsCorrect()
+        {
+            uut_.AddItem(1.2);
+            uut_.AddItem(2.6);
+            Assert.That(uut_.Total, Is.EqualTo(3.8));
+        }
+
     }
 }
